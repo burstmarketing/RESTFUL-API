f38429041dbeebafd97bd715f105f89f5c76193d
Dan LaManna <dan.lamanna@gmail.com>
2012-12-14 09:46:59 -0500

    Removing var dump of a sad developer.

--- Core/API.php
+++ Core/API.php
@@ -3,7 +3,7 @@
 
 abstract class Core_API {
   protected $_use_cache = false;
-  
+
   protected $_config;
   protected $_config_base_type;
 
@@ -17,17 +17,17 @@ abstract class Core_API {
 
 
   public function loadConfig( $file ){
-	if(is_readable( $file ) ){
-	  // skeleton code to add other config types
-	  // currently just defaults to json.
-	  switch( $this->_config_base_type ){
-	  default:
-		$this->_config = new Zend_Config_Json( $file, null, true );
-	  }
+        if(is_readable( $file ) ){
+          // skeleton code to add other config types
+          // currently just defaults to json.
+          switch( $this->_config_base_type ){
+          default:
+                $this->_config = new Zend_Config_Json( $file, null, true );
+          }
 
-	} else {
-	  throw new Exception( "Could not load file " . $file);
-	}
+        } else {
+          throw new Exception( "Could not load file " . $file);
+        }
   }
 
   public function getConfigs() {
@@ -35,27 +35,27 @@ abstract class Core_API {
   }
 
   public function getConfig( $uri ){
-	try {
-	  return $this->_config->getConfig( $uri );
-	} catch (Exception $e ) {
-	  return false;
-	}
+        try {
+          return $this->_config->getConfig( $uri );
+        } catch (Exception $e ) {
+          return false;
+        }
   }
 
   public function setConfig( $uri, $value ){
-	$this->_config->setConfig($uri, $value );
-	return $this;
+        $this->_config->setConfig($uri, $value );
+        return $this;
   }
 
 
   public function getService( $key ){
-	
-	if( $this->getConfig( 'services/' . $key ) ){
-	  return $this->getConfig( 'services/' . $key );
-	}
 
-	return false;
-	
+        if( $this->getConfig( 'services/' . $key ) ){
+          return $this->getConfig( 'services/' . $key );
+        }
+
+        return false;
+
   }
 
 
@@ -66,7 +66,7 @@ abstract class Core_API {
   //      in our new Core_RESTFUL_API class.
 
   protected function _underscore($name) {
-	return strtolower(preg_replace('/(.)([A-Z])/', "$1_$2", $name));
+        return strtolower(preg_replace('/(.)([A-Z])/', "$1_$2", $name));
   }
 
 
@@ -78,48 +78,47 @@ abstract class Core_API {
   //       returned as an array.
 
   public function __call($method, $args){
-    $matches = array();    
+    $matches = array();
     if( preg_match( '/^(load|post|put|delete)(.*)/', $method, $matches ) ){
-      var_dump($matches);
       $type = $matches[1];
       $key = $this->_underscore($matches[2]);
 
       try {
 
-	$request = $this->_getRequest();
+        $request = $this->_getRequest();
 
-	// @td make sure 'setAPI' is set up as an abstract function on
-	//     Core_API_Request
+        // @td make sure 'setAPI' is set up as an abstract function on
+        //     Core_API_Request
 
-	$request->setAPI($this);	
+        $request->setAPI($this);
 
-	$service = $this->getService( $key );
-	$service->key = $key;
+        $service = $this->getService( $key );
+        $service->key = $key;
 
-	if( ! isset( $service->url ) ){
-	  $service->url = $this->getConfig("defaults/url");
-	} else {
-	  throw new Exception( "could not locate a 'url' to use for this service");
-	}
+        if( ! isset( $service->url ) ){
+          $service->url = $this->getConfig("defaults/url");
+        } else {
+          throw new Exception( "could not locate a 'url' to use for this service");
+        }
 
-	$request->generateRequest( $service, $args );
-       	
+        $request->generateRequest( $service, $args );
 
-	$response = $this->_getResponse();
 
-	if( isset($service->classname) ){
-	  return $response->processRequest( $request, $service->classname );
-	}		  		  		  
-	
-	return $response->processRequest( $request );	
-	
+        $response = $this->_getResponse();
+
+        if( isset($service->classname) ){
+          return $response->processRequest( $request, $service->classname );
+        }
+
+        return $response->processRequest( $request );
+
       } catch (Core_Exception_Auth $e){
-	//	throw new Core_Exception_Auth('Y U NO AUTHENTICATE',0,$e);
-	throw $e;
+        //	throw new Core_Exception_Auth('Y U NO AUTHENTICATE',0,$e);
+        throw $e;
       }
     }
   }
-  
+
   abstract protected function _getRequest();
   abstract protected function _getResponse();
 
