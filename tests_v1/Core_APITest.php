3e252ab6b434d6485929e19893f4be6a6cb54f6e
Dan LaManna <dan.lamanna@gmail.com>
2012-12-20 05:31:10 -0500

    Switching tests to more agile documentation style.

--- tests_v1/Core_APITest.php
+++ tests_v1/Core_APITest.php
@@ -22,100 +22,89 @@ class Mock_Core_API extends Core_API {
  */
 class Core_APITest extends PHPUnit_Framework_TestCase {
     /**
-     * @var Core_API
+     * @var Mock_Core_API
      */
     protected $object;
 
-    /**
-     * Sets up the fixture, for example, opens a network connection.
-     * This method is called before a test is executed.
-     */
     protected function setUp() {
         $this->object = new Mock_Core_API;
     }
 
-    /**
-     * @covers Core_API::useCache
-     * @todo Implement testUseCache().
-     */
-    public function testUseCache() {
-      // Initially false..
+    public function testUseCacheIsInitiallyFalse() {
       $this->assertFalse($this->object->useCache());
+    }
 
+    public function testUseCacheReturnsCoreApiWhenSet() {
       $this->assertInstanceOf('Core_API', $this->object->useCache(true));
+    }
+
+    public function testUseCacheSetter() {
+      $this->object->useCache(true);
 
       $this->assertTrue($this->object->useCache());
     }
 
-    /**
-     * @covers Core_API::loadConfig
-     * @todo Implement testLoadConfig().
-     */
-    public function testLoadConfig() {
-      // Ensure our config object isn't an object yet
-      $this->assertEquals($this->object->getRawConfig(), null);
+    public function testConfigIsInitiallyNull() {
+      $this->assertNull($this->object->getRawConfig());
+    }
 
+    public function testConfigThrowsExceptionForNonJsonFiles() {
       // Try loading a non-json file into config
-      try {
-        $this->object->loadConfig(ASSEMBLA_REST_API_ROOT . '/Autoload.php');
+      $this->setExpectedException('Zend_Json_Exception');
 
-        $this->fail('Expected Zend_Json_Exception');
-      } catch (Zend_Json_Exception $e) {}
+      $this->object->loadConfig(ASSEMBLA_REST_API_ROOT . '/Autoload.php');
+    }
 
-      // Should still be empty
-      $this->assertEquals($this->object->getRawConfig(), null);
+    public function testConfigThrowsExceptionForUnreadableFile() {
+      $this->setExpectedException('Assembla_Exception');
 
-      // Try loading a non-readable file into config
-      try {
-        $this->object->loadConfig('some-non-existent-file');
+      $this->object->loadConfig('some-non-existent-file');
+    }
 
-        $this->fail('Expected Assembla_Exception for unreadable file.');
-      } catch (Zend_Json_Exception $e) {
-        $this->fail('Expected Assembla_Exception for unreadable file.');
-      } catch (Assembla_Exception $e) {}
-
+    public function testLoadedConfigIsOfTypeZendConfigJson() {
       // Real config, _config should be an instance of Zend_Config_Json
       $this->object->loadConfig(ASSEMBLA_REST_API_ROOT . '/Assembla/etc/config.json');
       $this->assertTrue($this->object->getRawConfig() instanceof Zend_Config_Json);
     }
 
-    /**
-     * @covers Core_API::getConfigs
-     * @todo Implement testGetConfigs().
-     */
-    public function testGetConfigs() {
+    public function testGetConfigsReturnsArray() {
       $this->object->loadConfig(ASSEMBLA_REST_API_ROOT . '/Assembla/etc/config.json');
 
-      // getConfigs needs to return an array of Zend_Config objects
-      $getConfigs = $this->object->getConfigs();
+      $this->assertTrue(is_array($this->object->getConfigs()));
+    }
 
-      $this->assertTrue(is_array($getConfigs));
+    /**
+     * @depends testGetConfigsReturnsArray
+     **/
+    public function testGetConfigsIsArrayOfZendConfigObjects() {
+      $this->object->loadConfig(ASSEMBLA_REST_API_ROOT . '/Assembla/etc/config.json');
 
-      foreach ($getConfigs as $config) {
+      foreach ($this->object->getConfigs() as $config) {
         $this->assertInstanceOf('Zend_Config', $config);
       }
     }
 
-    /**
-     * @covers Core_API::getConfig
-     * @todo Implement testGetConfig().
-     */
-    public function testGetConfig()
-    {
+    public function testGetConfigExistingUri() {
       $this->object->loadConfig(ASSEMBLA_REST_API_ROOT . '/Assembla/etc/config.json');
 
       $this->assertEquals($this->object->getConfig('defaults/url'), 'https://api.assembla.com');
-
-      $this->assertEquals($this->object->getConfig('non-existent-key-with-no-slash'), false);
-
-      // Throws fatal..
-      //$this->assertEquals($this->object->getConfig('non-existent-key-with-a-/'), false);
     }
 
-    /**
-     * @covers Core_API::setConfig
-     * @todo Implement testSetConfig().
-     */
+    public function testGetConfigNonExistentUriReturnsFalse() {
+      $this->object->loadConfig(ASSEMBLA_REST_API_ROOT . '/Assembla/etc/config.json');
+
+      $this->assertFalse($this->object->getConfig('non-existent-key-with-no-slash'));
+    }
+
+    public function testGetConfigNonExistentUriWithSlashReturnsFalse() {
+      $this->object->loadConfig(ASSEMBLA_REST_API_ROOT . '/Assembla/etc/config.json');
+
+      // Throws fatal error
+      //$this->assertEquals($this->object->getConfig('non-existent-key-with-a-/'), false);
+
+      $this->markTestIncomplete('Test not yet implemented.');
+    }
+
     public function testSetConfig() {
       $this->object->loadConfig(ASSEMBLA_REST_API_ROOT . '/Assembla/etc/config.json');
 
@@ -130,29 +119,24 @@ class Core_APITest extends PHPUnit_Framework_TestCase {
       $this->assertEquals($this->object->getConfig('a-stdclass-obj'), new StdClass);
     }
 
-    /**
-     * @covers Core_API::getService
-     * @todo Implement testGetService().
-     */
-    public function testGetService() {
+    public function testGetServiceReturnsFalseForNonExistentServices() {
       $this->object->loadConfig(ASSEMBLA_REST_API_ROOT . '/Assembla/etc/config.json');
 
       // Make sure false is returned for nonexistent services
       $this->assertFalse($this->object->getService('some-nonexistent-service'));
+    }
+
+    public function testGetServiceReturnsZendConfigObject() {
+      $this->object->loadConfig(ASSEMBLA_REST_API_ROOT . '/Assembla/etc/config.json');
 
       // Make sure it returns a Zend_Config object for an existing service
       $this->assertInstanceOf('Zend_Config', $this->object->getService('ticket_by_id'));
     }
 
-    /**
-     * @covers Core_API::__call
-     * @todo Implement test__call().
-     */
-    public function test__call() {
-      try {
-        $this->object->someExampleMethod();
+    public function testCallThrowsExceptionForInvalidMethodPrefixes() {
+      $this->setExpectedException('Assembla_Exception');
 
-        $this->fail('Should throw Assembla_Exception for Invalid Method.');
-      } catch (Assembla_Exception $e) {}
+      // anything other than post,put,delete,load are invalid prefixes
+      $this->object->someExampleMethod();
     }
 }
