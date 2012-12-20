8cde1c0160348ef5aab2b798c7101fc4c513d84e
Dan LaManna <dan.lamanna@gmail.com>
2012-12-19 22:07:31 -0500

    Formatting.

--- Assembla/API.php
+++ Assembla/API.php
@@ -1,6 +1,5 @@
 <?php
 
-
 class Assembla_API extends Core_API {
   protected $_request_class = "Assembla_API_V1_Request";
   protected $_response_class = "Assembla_API_V1_Response";
@@ -9,33 +8,32 @@ class Assembla_API extends Core_API {
   // a little sneaky if '$file' is already a config object
   // set _config to that,  else load $file as if it were a file
   public function loadConfig( $file ){
-        if( is_object($file) && $file instanceof Zend_Config ){
-          $this->_config = $file;
-        } else {
-          parent::loadConfig( $file );
-        }
+    if( is_object($file) && $file instanceof Zend_Config ){
+      $this->_config = $file;
+    } else {
+      parent::loadConfig( $file );
+    }
 
-        return $this;
+    return $this;
   }
 
   public function getUserName(){
-        return $this->getConfig('credentials/username');
+    return $this->getConfig('credentials/username');
   }
 
   public function getPassword(){
-        return $this->getConfig('credentials/password');
+    return $this->getConfig('credentials/password');
   }
 
   public function setUserName( $username ){
-        $this->setConfig('credentials/username', $username );
-        return $this;
+    $this->setConfig('credentials/username', $username );
+    return $this;
   }
 
   public function setPassword( $password ){
-        $this->setConfig('credentials/password', $password );
-        return $this;
+    $this->setConfig('credentials/password', $password );
+    return $this;
   }
-
 
   public function getApiKey(){
     return $this->getConfig('credentials/api_key');
@@ -55,8 +53,6 @@ class Assembla_API extends Core_API {
     return $this;
   }
 
-
-
   protected function _getRequest(){
     $request = new $this->_request_class;
     $request->useCache( $this->useCache() );
@@ -67,8 +63,4 @@ class Assembla_API extends Core_API {
     $response = new $this->_response_class;
     return $response;
   }
-
-  }
-
-
-?>
+}
