4b1013f83232383b0b93deff9a8b208c7ae04cb7
Dan LaManna <dan.lamanna@gmail.com>
2012-12-23 14:58:48 -0500

    Refactoring response.

--- Assembla/API/V1/Response.php
+++ Assembla/API/V1/Response.php
@@ -1,36 +1,40 @@
 <?php
 
-// @todo shouldn't this extend Core_API_Response_Json?
-class Assembla_API_V1_Response  extends Core_API_Response_XML {
+class Assembla_API_V1_Response  extends Core_API_Response_Json {
 
-  public function processRequest($request, $classname = "Core_Object" ){
-        $http_response = $request->send();
-        $data = json_decode( $http_response, true );
-        if( ! isset($data['errors']) ){
-          switch( $request->getType() ){
-          case 'PUT':
-          case 'POST':
-          case 'DELETE':
-            $message = new Core_Object;
-            $message->setSuccess(1)->setBody( new Core_Object($data) );
-            return $message;
-            break;
-          default:
-            $class = new $classname();
-            return $class->setData( $data );
-            break;
+  public function processRequest(Core_API_Request $request, $classname = "Core_Object" ){
+    $http_response = $request->send();
+    $data          = json_decode($http_response, true);
+
+    if (!isset($data['errors'])) {
+      switch ($request->getType()) {
+      case 'PUT':
+      case 'POST':
+      case 'DELETE':
+        $message = new Core_Object;
+        $message->setSuccess(1)
+                ->setBody(new Core_Object($data));
+
+        return $message;
+        break;
+
+      default:
+        $class = new $classname;
+        return $class->setData($data);
+        break;
+      } else {
+        $error = "Errors were encountered: \n";
+
+        foreach ($data['errors'] as $e) {
+          if ($e == 'HTTP Basic: Access denied.') {
+            throw new Core_Exception_Auth('Authentication credentials failed.');
+          } else {
+            $error .= ' ' . $e . ' ';
           }
-        } else {
-          // this is wrong,  need to get some errors to fix it,  definately
-          $err = "Errors were encountered:";
-          foreach( $data['errors'] as $e ){
-            if( $e == "HTTP Basic: Access denied." ){
-              throw new Core_Exception_Auth( "Authentical credentials failed!" );
-            } else {
-              $err .= " " . $e . " ";
-            }
-          }
-          throw new Exception( $err );
         }
+
+        throw new Assembla_Exception($error);
+      }
+    }
   }
 }
