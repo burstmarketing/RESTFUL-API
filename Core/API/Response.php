b96f1df2d5a639af131b21c5015dc71821823aae
Dan LaManna <dan.lamanna@gmail.com>
2012-12-23 16:57:37 -0500

    More strict type checking.

--- Core/API/Response.php
+++ Core/API/Response.php
@@ -1,9 +1,6 @@
 <?php
-  // Response objects take xml and return a class that 
-  // Represents that xml object.  
+
 abstract class Core_API_Response extends Core_Object {
 
-  abstract public function processRequest( $http_response, $classname = "Core_Object" );
-
-  }
-?>
+  abstract public function processRequest(Core_API_Request  $http_response, $classname = "Core_Object" );
+}
