a01a81e82c48be9c359beb41c594d38ae537b043
Dan LaManna <dan.lamanna@gmail.com>
2012-12-23 23:20:20 -0500

    Whitespace.

--- Assembla/Collection/Ticket.php
+++ Assembla/Collection/Ticket.php
@@ -1,30 +1,30 @@
 <?php
 class Assembla_Collection_Ticket extends Assembla_Collection_Abstract {
-  
+
   protected function _getModelClassName(){
     return "Assembla_Model_Ticket";
   }
-  
-  
+
+
 
 
   public function getSpaceTickets( $space_id ){
     return new Blast_Assembla_Collection_Ticket( array_filter( $this->getCollection(),
-							       function( $ticket ) use ($space_id) { 
-								return $ticket->getSpaceId() == $space_id;
-							      } ) );
+                                                               function( $ticket ) use ($space_id) {
+                                                                return $ticket->getSpaceId() == $space_id;
+                                                              } ) );
   }
 
   public function getUniqueSpaceIdsFromTickets(){
     return array_unique( array_map( function( $ticket ){ return $ticket->getSpaceId(); }, $this->getCollection() ) );
   }
-  
+
 
   public function asArraySortedBySpaceId(){
     $space_tickets = array();
     $class_name = get_class($this);
 
-    // chunk out the tickets for each space                                                                     
+    // chunk out the tickets for each space
     foreach( $this->getCollection() AS $ticket ){
       if(! array_key_exists( $ticket->getSpaceId(), $space_tickets ) ){
         $space_tickets[$ticket->getSpaceId()] = new $class_name;
@@ -36,20 +36,20 @@ class Assembla_Collection_Ticket extends Assembla_Collection_Abstract {
 
 
   protected function _appendPagedReport( $id, $use_cache ){
-    $i = 0; 
-    do{ 
-      $i++; 
+    $i = 0;
+    do{
+      $i++;
       $length = count($this);
 
       $request = $this->_getAPI($use_cache)->generateRequest("paged_portfolio_ticket_report", array( $id, $i ) );
       $http_response = $request->send();
 
       if( simplexml_load_string($http_response) ){
-	$element = new Assembla_API_XML_Element( $http_response );
-	$this->load($element);
+        $element = new Assembla_API_XML_Element( $http_response );
+        $this->load($element);
       }
-      // asCanonicalArray will return "" if empty - this means 
-      // we're out of tickets so break the loop.  
+      // asCanonicalArray will return "" if empty - this means
+      // we're out of tickets so break the loop.
     } while( (bool) $element->asCanonicalArray() );
   }
 
@@ -65,7 +65,7 @@ class Assembla_Collection_Ticket extends Assembla_Collection_Abstract {
     $this->_filters["statusFilter"] = array( "status" => $status );
     return $this;
   }
-  
+
 
   private function _convertTicketStatus( $status_element ){
     if( (string) $status_element == "Array" ):
@@ -74,15 +74,15 @@ class Assembla_Collection_Ticket extends Assembla_Collection_Abstract {
       return (string) $status_element;
     endif;
   }
-  
+
   protected function statusFilter( $element, $status ){
-    if( !is_array($status) ){ 
-      $status = array($status); 
+    if( !is_array($status) ){
+      $status = array($status);
     }
-    
+
     foreach( $status AS $s ){
-      if( $this->_convertTicketStatus( $element->status ) == $s ){
-	return true;
+      if ($element['status'] == $s) {
+        return true;
       }
     }
 
@@ -90,32 +90,30 @@ class Assembla_Collection_Ticket extends Assembla_Collection_Abstract {
   }
 
   protected function excludeStatusFilter( $element, $status ){
-    if( !is_array($status) ){ 
-      $status = array($status); 
-    }
-    
+    $status = (array) $status;
+
     if( !in_array( $this->_convertTicketStatus( $element->status ), $status ) ){
       return true;
     }
 
     return false;
   }
-  
+
 
   public function addExcludeStatusFilter( $status ){
     $this->_filters["excludeStatusFilter"] = array( "status" => $status );
     return $this;
   }
-  
 
-  
-  // Due Date filter                                                                                                                                                                     
+
+
+  // Due Date filter
   public function addHasDueDateFilter(){
     $this->_filters['hasDueDateFilter'] = array();
     return $this;
   }
-  
-  protected function hasDueDateFilter( $element ){    
+
+  protected function hasDueDateFilter( $element ){
     if( $element->xpath(".//custom-field[@name='Due Date']") ){
       return true;
     } else {
@@ -123,23 +121,23 @@ class Assembla_Collection_Ticket extends Assembla_Collection_Abstract {
     }
   }
 
-  // Date Filter functions                                                                                                                                                               
+  // Date Filter functions
   public function addDateAfterFilter( $date, $date_string ){
     $this->_filters["dateAfter"] = array( "date" => $date, "date_string" => $date_string );
     return $this;
   }
-  
-  
+
+
   protected function dateAfter( $element, $date, $date_string ){
     $element_date = new DateTime( (string) $element->$date_string );
     return (bool)( $element_date->getTimeStamp() > $date->getTimeStamp() );
   }
-  
+
   public function addDateBeforeFilter( $date, $date_string ){
     $this->_filters["dateBefore"] = array( "date" => $date, "date_string" => $date_string );
     return $this;
   }
-  
+
   protected function dateBefore( $element, $date, $date_string ){
     $element_date = new DateTime( (string) $element->$date_string );
     return (bool)( $element_date->getTimeStamp() < $date->getTimeStamp() );
@@ -149,15 +147,15 @@ class Assembla_Collection_Ticket extends Assembla_Collection_Abstract {
     $this->_filters["lastNumDaysFilter"] = array( "days" => $days, "date_string" => $date_string );
     return $this;
   }
-  
+
   protected function lastNumDaysFilter( $element, $days, $date_string ){
     $element_date = new DateTime( (string) $element->$date_string );
     $interval = $element_date->diff( new DateTime('') );
-    
+
     return (bool)( $interval->days < $days );
-    
+
   }
-  
+
 
 
 
