35ec4d31a1c1bf578efe25c8ef81d2bf952e91ef
Dan LaManna <dan.lamanna@gmail.com>
2012-12-23 23:19:46 -0500

    Removing as canonical bit, as we're working with arrays already.

--- Assembla/Model/Abstract.php
+++ Assembla/Model/Abstract.php
@@ -3,31 +3,31 @@
 class Assembla_Model_Abstract extends Core_Model {
 
   static public function getTagName(){
-	return "";
+        return "";
   }
   public function getInvalidKeys() {
-	return array();
+        return array();
   }
 
   public function load( $element ){
-	$this->setData( $element->asCanonicalArray() );
-	return $this;
+        $this->setData($element);
+        return $this;
   }
-  
+
   //NOTE:  this should probably return recursive array keys
   protected function _getDataKeys(){
-	return array_keys( $this->getData() );
+        return array_keys( $this->getData() );
   }
 
   public function toXml($arrAttributes = array(), $rootName = ''){
 
-	if( !$rootName ){
-	  $rootName = $this::getTagName();
-	}
+        if( !$rootName ){
+          $rootName = $this::getTagName();
+        }
 
-	$arrAttributes = array_diff( $this->_getDataKeys(), $this->getInvalidKeys() );
+        $arrAttributes = array_diff( $this->_getDataKeys(), $this->getInvalidKeys() );
 
-	return parent::toXml( $arrAttributes, $rootName );
+        return parent::toXml( $arrAttributes, $rootName );
   }
 
   }
