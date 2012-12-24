b52848d97c24f62bb0c2b17dbf17af6c37218282
Dan LaManna <dan.lamanna@gmail.com>
2012-12-23 23:20:43 -0500

    Moving filtering logic into a protected method.

--- Assembla/Collection/Abstract.php
+++ Assembla/Collection/Abstract.php
@@ -15,43 +15,42 @@ abstract class Assembla_Collection_Abstract extends Core_Collection {
 
   protected function _getModelElementTag(){
     $model_name = $this->_getModelClassName();
-	return $model_name::getTagName();
+        return $model_name::getTagName();
   }
-  
+
   protected function _getModelClassName(){
-	// This is almost certainly NOT what we want
-	// please override this function in inherited
-	// classes
-	return "Core_Model";
+        // This is almost certainly NOT what we want
+        // please override this function in inherited
+        // classes
+        return "Core_Model";
   }
 
-  public function load( $element ){
-	$classname = $this->_getModelClassName();
-	$_elements = $element->xpath( $this->_getModelElementTag() );
-	foreach( $_elements AS $_element ){
-	  
-	  $sentinal = true;
-	  foreach( $this->_filters AS $_func_name => $_args ){
-	    array_unshift($_args, $_element);
-	    if( ! $sentinal = call_user_func_array( array( $this, $_func_name), $_args ) ){
-	      break;
-	    }
-	  }
+  public function load(array $data) {
+    $this->addStatusFilter('Test');
 
-	  if( $sentinal ){
-	    $model = new $classname();
-	    $model->load( $_element );	    
-	    $this->offsetSet( "", $model );
-	  }
+    $model_class = $this->_getModelClassName();
 
-	}
-	
-	return $this;
+    foreach ($this->_filterData($data) as $model_data) {
+      $model = new $model_class;
+      $model->load($model_data);
+      $this->offsetSet('', $model);
+    }
+
+    return $this;
   }
 
+  protected function _filterData(array $data) {
+    foreach ($data as $key => &$val) {
+      foreach ($this->_filters as $_func_name => $_args) {
+        /* Throw $val at the beginning of $_args so the filter
+           method gets the model data, and the args */
+        array_unshift($_args, $val);
+        if (!call_user_func_array(array($this, $_func_name), $_args)) {
+          unset($data[$key]);
+        }
+      }
+    }
 
-
+    return $data;
+  }
 }
-
-
-?>
