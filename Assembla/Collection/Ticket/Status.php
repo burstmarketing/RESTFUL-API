db85a17f80a28d7a5ab1efc7cdf2355eed9f2baa
Dan LaManna <dan.lamanna@gmail.com>
2012-12-25 14:48:21 -0500

    Adding ticket status model and collection.

---
+++ Assembla/Collection/Ticket/Status.php
@@ -0,0 +1,8 @@
+<?php
+
+class Assembla_Collection_Ticket_Status extends Assembla_Collection_Abstract {
+
+  protected function _getModelClassName() {
+    return "Assembla_Model_Ticket_Status";
+  }
+}
---
+++ Assembla/Model/Ticket/Status.php
@@ -0,0 +1,10 @@
+<?php
+
+class Assembla_Model_Ticket_Status extends Assembla_Model_Abstract {
+
+  public function load($element) {
+    parent::load($element);
+
+    return $this;
+  }
+}
