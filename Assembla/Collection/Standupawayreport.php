6e4a5cbb5e5cc73ed88ab279034a5b45b55b374c
Dan LaManna <dan.lamanna@gmail.com>
2012-12-25 17:50:14 -0500

    Adding standup away report model/collection.

---
+++ Assembla/Collection/Standupawayreport.php
@@ -0,0 +1,8 @@
+<?php
+
+class Assembla_Collection_Standupawayreport extends Assembla_Collection_Abstract {
+
+  protected function _getModelClassName() {
+    return 'Assembla_Model_Standupawayreport';
+  }
+}
