61ce30475958dd5e73209c058669c28298fbf0dc
Dan LaManna <dan.lamanna@gmail.com>
2012-12-25 16:38:27 -0500

    Adding activity model/collection.

---
+++ Assembla/Collection/Activity.php
@@ -0,0 +1,8 @@
+<?php
+
+class Assembla_Collection_Activity extends Assembla_Collection_Abstract {
+
+  protected function _getModelClassName() {
+    return 'Assembla_Model_Activity';
+  }
+}
---
+++ Assembla/Model/Activity.php
@@ -0,0 +1,8 @@
+<?php
+
+class Assembla_Model_Activity extends Assembla_Model_Abstract {
+
+  public function load($element) {
+    return parent::load($element);
+  }
+}
