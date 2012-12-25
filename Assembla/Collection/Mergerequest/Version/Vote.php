810d11e5d363bba64e802827df6a47cc95a5b8e5
Dan LaManna <dan.lamanna@gmail.com>
2012-12-25 17:47:36 -0500

    Adding merge request version vote model/collection.

---
+++ Assembla/Collection/Mergerequest/Version/Vote.php
@@ -0,0 +1,8 @@
+<?php
+
+class Assembla_Collection_Mergerequest_Version_Vote extends Assembla_Collection_Abstract {
+
+  protected function _getModelClassName() {
+    return 'Assembla_Model_Mergerequest_Version_Vote';
+  }
+}
---
+++ Assembla/Model/Mergerequest/Version/Vote.php
@@ -0,0 +1,8 @@
+<?php
+
+class Assembla_Model_Mergerequest_Version_Vote extends Assembla_Model_Abstract {
+
+  public function load($element) {
+    return parent::load($element);
+  }
+}
