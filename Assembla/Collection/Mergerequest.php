30d5f8394b1f3b1556cf9677b9db1efd8f3859cc
Dan LaManna <dan.lamanna@gmail.com>
2012-12-25 17:46:05 -0500

    Adding merge request model/collection.

---
+++ Assembla/Collection/Mergerequest.php
@@ -0,0 +1,8 @@
+<?php
+
+class Assembla_Collection_Mergerequest extends Assembla_Collection_Abstract {
+
+  protected function _getModelClassName() {
+    return 'Assembla_Model_Mergerequest';
+  }
+}
