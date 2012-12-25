02c4a6c1d247f255b458924bbb070c234132d697
Dan LaManna <dan.lamanna@gmail.com>
2012-12-25 17:12:30 -0500

    Adding space tool model/collection.

--- Assembla/Collection/Space/Tool.php
+++ Assembla/Collection/Space/Tool.php
@@ -1,14 +1,8 @@
 <?php
+
 class Assembla_Collection_Space_Tool extends Assembla_Collection_Abstract {
-  
-  protected function _getModelElementTag(){
-	return 'spaces-tool';
-  }
 
-  protected function _getModelClassName(){
-	return "Assembla_Model_Space_Tool";
+  protected function _getModelClassName() {
+    return "Assembla_Model_Space_Tool";
   }
-
-
-  }
-?>
+}
