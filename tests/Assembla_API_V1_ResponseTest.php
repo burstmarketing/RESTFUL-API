644abf935373680d1aca1da4ba6ca701fd2667e7
Dan LaManna <dan.lamanna@gmail.com>
2012-12-26 22:50:34 -0500

    Allowing of testing requests/responses by the use of JSON fixtures.
    In theory, we want every single part of the request/response lifecycle for an API call to remain exactly the same, with the exception of the send method on the request object, that needs to return pre-defined
    JSON instead of calling out to Assemblas server, so since the object is created in the middle of the request, we need to make two Mock objects:
    
    - Mock_Assembla_API - This only has one method _getRequest (the method that creates the object), and forces it to return a Mock_Assembla_API_V1_Request which redefines our send method.
    
    - Mock_Assembla_API_V1_Request contains a uri_fixture_mapping, which tells the send method where to look for a fixture to return rather than calling it out to the service. To avoid throwing hundreds of API calls during a test, the test is marked as skipped if it can't find/read the proper fixture file.

--- tests/Assembla_API_V1_ResponseTest.php
+++ tests/Assembla_API_V1_ResponseTest.php
@@ -7,12 +7,56 @@ if (!defined("ASSEMBLA_REST_API_ROOT")) {
 require_once ASSEMBLA_REST_API_ROOT . '/Autoload.php';
 require_once ASSEMBLA_REST_API_ROOT . '/Assembla/API/V1/Response.php';
 
-class Dummy_API_V1_Response extends Assembla_API_V1_Response {}
+class Mock_Assembla_API extends Assembla_API {
+
+  protected function _getRequest() {
+    return new Mock_Assembla_API_V1_Request;
+  }
+}
+
+class Mock_Assembla_API_V1_Request extends Assembla_API_V1_Request {
+
+  public $uri_fixture_mapping = array(
+    '/v1/activity.json' => '/fixtures/activity.json');
+
+  /**
+   * If it has a URI, it exists in the fixture mapping, and it's readable,
+   * it returns the fixtures contents, otherwise it throws an error and marks
+   * the test as skipped.
+   **/
+  public function send() {
+    if ($uri = $this->getUri()) {
+      if (!array_key_exists($uri, $this->uri_fixture_mapping)) {
+        $this->markTestSkipped(sprintf('No Fixture found for URI: %s.', $uri));
+      } elseif (!is_readable(ASSEMBLA_REST_API_ROOT . '/tests' . $this->uri_fixture_mapping[$uri])) {
+        $this->markTestSkipped(sprintf('Fixture file unreadable: %s.', $this->uri_fixture_mapping[$uri]));
+      } else {
+        return file_get_contents(ASSEMBLA_REST_API_ROOT . '/tests' . $this->uri_fixture_mapping[$uri]);
+      }
+    } else {
+      $this->markTestSkipped('No URI found on Request Object.');
+    }
+  }
+}
 
 class Assembla_API_V1_ResponseTest extends PHPUnit_Framework_TestCase {
 
+  public function setUp() {
+    $this->api = new Mock_Assembla_API;
+    $this->api->loadConfig(ASSEMBLA_REST_API_ROOT . '/Assembla/etc/config.json');
+  }
+
+  public function testLoadActivity() {
+    $activity_collection = $this->api->loadActivity();
+
+    $this->assertInstanceOf('Assembla_Collection_Activity', $activity_collection);
+
+    foreach ($activity_collection as $activity) {
+      $this->assertInstanceOf('Assembla_Model_Activity', $activity);
+    }
+  }
+
   public function testProcessRequest() {
     $this->markTestIncomplete('This test has not been implemented yet.');
-    // Setup mock request objects, and test possible responses
   }
 }
