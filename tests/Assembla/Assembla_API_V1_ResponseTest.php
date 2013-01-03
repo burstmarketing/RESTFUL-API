2d35b785688af76628c1bae048bdffdbe4003550
mdame <mdame@burstmarketing.net>
2013-01-03 14:21:09 -0500

    Writing new Assembla service tests re #182

--- tests/Assembla/Assembla_API_V1_ResponseTest.php
+++ tests/Assembla/Assembla_API_V1_ResponseTest.php
@@ -19,7 +19,19 @@ class Mock_Assembla_API_V1_Request extends Assembla_API_V1_Request {
   public $uri_fixture_mapping = array(
 				      '/v1/activity.json' => '/fixtures/activity.json',
 				      '/v1/spaces.json' => '/fixtures/spaces.json',
-				      '/v1/tickets.json' => '/fixtures/tickets.json');
+				      '/v1/spaces/foo/tickets.json' => '/fixtures/tickets.json',
+				      '/v1/user.json' => '/fixtures/user.json',
+				      '/v1/users/foo.json' => '/fixtures/user.json',
+				      '/v1/spaces/foo/users.json' => '/fixtures/spaces.json',
+				      '/v1/spaces/foo.json' => '/fixtures/space.json',
+				      '/v1/spaces/foo/user_roles.json' => '/fixtures/user_roles.json',
+				      '/v1/spaces/foo/user_roles/bar.json' => '/fixtures/user_roles_bar.json',
+				      '/v1/spaces/foo/tickets/bar.json' => '/fixtures/ticket.json',
+				      '/v1/spaces/foo/tickets/id/bar.json' =>'/fixtures/ticket.json',
+				      '/v1/spaces/foo/tickets/statuses.json' => '/fixtures/ticketstatuses.json',
+				      '/v1/spaces/foo/tickets/statuses/bar.json' => '/fixtures/ticketstatuses.json',
+
+				      );
 
   /**
    * If it has a URI, it exists in the fixture mapping, and it's readable,
@@ -59,19 +71,72 @@ class Assembla_API_V1_ResponseTest extends PHPUnit_Framework_TestCase {
   }
 
   public function testLoadTickets() {
-    $ticket_collection = $this->api->loadTickets();
-    $this->assertInstanceOf('Assembla_Collection_Tickets', $ticket_collection);
+    $ticket_collection = $this->api->loadTickets(array('space_id' => 'foo'));
+    $this->assertInstanceOf('Assembla_Collection_Ticket', $ticket_collection);
 
     foreach ($ticket_collection as $ticket) {
       $this->assertInstanceOf('Assembla_Model_Ticket', $ticket);
     }
-    $this->markTestIncomplete('Test incomplete');
+  }
+
+  public function testLoadTicketByNumber() {
+    $ticket = $this->api->loadTicketByNumber(array('space_id' => 'foo', 'number' => 'bar'));
+    $this->assertInstanceOf('Assembla_Model_Ticket', $ticket);
+  }
+
+  public function testLoadTicketById() {
+    $ticket = $this->api->loadTicketById(array('space_id' => 'foo', 'id' => 'bar'));
+    $this->assertInstanceOf('Assembla_Model_Ticket', $ticket);
   }
 
   public function testLoadSpaces() {
     $space_collection = $this->api->loadSpaces();
     $this->assertInstanceOf('Assembla_Collection_Space', $space_collection);
+
+    foreach ($space_collection as $space) {
+      $this->assertInstanceOf('Assembla_Model_Space', $space);
+    }
    }
+
+  public function testLoadSpace() {
+    $space = $this->api->loadSpace(array('id' => 'foo'));
+    $this->assertInstanceOf('Assembla_Model_Space', $space);
+  }
+
+  public function testLoadUser() {
+    $user = $this->api->loadUser();
+    $this->assertInstanceOf('Assembla_Model_User', $user);
+  }
+
+  public function testLoadShowUser() {
+    $user = $this->api->loadShowUser(array('id_or_login' => 'foo'));
+    $this->assertInstanceOf('Assembla_Model_User', $user);
+  }
+
+  public function testLoadUsersBySpace() {
+    $users = $this->api->loadUsersBySpace(array('space_id' => 'foo'));
+    $this->assertInstanceOf('Assembla_Collection_User', $users);
+  }
+
+  public function testLoadUserRoles() {
+    $users = $this->api->loadUserRoles(array('space_id' => 'foo'));
+    $this->assertInstanceOf('Assembla_Collection_Userrole', $users);
+  }
+
+  public function testLoadUserRole() {
+    $user = $this->api->loadUserRole(array('space_id' => 'foo', 'id' => 'bar'));
+    $this->assertInstanceOf('Assembla_Model_Userrole', $user);
+  }
+
+  public function testLoadSpaceTicketStatuses() {
+    $statuses = $this->api->loadSpaceTicketStatuses(array('space_id' => 'foo'));
+    $this->assertInstanceOf('Assembla_Collection_Ticket_Status', $statuses);
+  }
+
+  public function testLoadSpaceTicketStatusById() {
+    $status = $this->api->loadSpaceTicketStatusById(array('space_id' => 'foo', 'id' => 'bar'));
+    $this->assertInstanceOf('Assembla_Model_Ticket_Status', $status);
+  }
 
   public function testProcessRequest() {
     $this->assertInstanceOf('Assembla_Collection_Activity', $this->api->loadActivity());
