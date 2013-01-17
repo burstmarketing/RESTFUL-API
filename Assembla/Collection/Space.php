deb700a36e9379d7d56095f0e7aaaff2c77c69a5
Christopher Kotfila <kotfic@gmail.com>
2013-01-17 08:56:31 -0500

    fixing whitespace issues

--- Assembla/Collection/Space.php
+++ Assembla/Collection/Space.php
@@ -1,12 +1,12 @@
 <?php
 class Assembla_Collection_Space extends Assembla_Collection_Abstract {
-  
+
   protected function _getModelElementTag(){
-	return "space";
+    return "space";
   }
 
   protected function _getModelClassName(){
-	return "Assembla_Model_Space";
+    return "Assembla_Model_Space";
   }
 
   public function loadSpacesByTicketCollection( $tickets, $use_cache = false ){
@@ -19,15 +19,15 @@ class Assembla_Collection_Space extends Assembla_Collection_Abstract {
       $current_space_ids = $this->getCollectionIds();
 
       foreach( $space_ids AS $space_id => $ticket_collection ){
-	if( !in_array( $space_id, $current_space_ids ) ){
-	  $model = new $model_name;
-	  $model->loadById($space_id, $use_cache);	
-	  $model->setTickets( $ticket_collection );
-	  $this->offsetSet("", $model );
-	}
+        if( !in_array( $space_id, $current_space_ids ) ){
+          $model = new $model_name;
+          $model->loadById($space_id, $use_cache);
+          $model->setTickets( $ticket_collection );
+          $this->offsetSet("", $model );
+        }
       }
-      
-  }
+
+    }
     return $this;
   }
 
@@ -38,23 +38,23 @@ class Assembla_Collection_Space extends Assembla_Collection_Abstract {
 
   public function loadByIds( $space_ids, $use_cache = false ){
     if( !is_array( $space_ids ) ){ $space_ids = array( $space_ids ); }
-    
-    $model_name = $this->_getModelClassName(); 
+
+    $model_name = $this->_getModelClassName();
 
     foreach( $space_ids AS $space_id ){
       if( is_string($space_id) ){
-	$model = new $model_name();
-	$this->offsetSet( "", $model->loadById($space_id, $use_cache) );
+        $model = new $model_name();
+        $this->offsetSet( "", $model->loadById($space_id, $use_cache) );
       }
     }
-    
+
     return $this;
-    
-  }
-  
-  
 
   }
+
+
+
+}
 
 
 ?>
--- Assembla/Collection/Ticket.php
+++ Assembla/Collection/Ticket.php
@@ -1,6 +1,6 @@
 <?php
 class Assembla_Collection_Ticket extends Assembla_Collection_Abstract {
-  
+
 
   // cache spaces loaded by the component filter so we don't have
   // to load the space and its components more than once.
@@ -8,25 +8,25 @@ class Assembla_Collection_Ticket extends Assembla_Collection_Abstract {
 
   protected function _getModelClassName(){
     return "Assembla_Model_Ticket";
-  }   
+  }
 
   public function getSpaceTickets( $space_id ){
     return new Blast_Assembla_Collection_Ticket( array_filter( $this->getCollection(),
-							       function( $ticket ) use ($space_id) { 
-								return $ticket->getSpaceId() == $space_id;
-							      } ) );
+                                                               function( $ticket ) use ($space_id) {
+                                                             return $ticket->getSpaceId() == $space_id;
+                                                           } ) );
   }
 
   public function getUniqueSpaceIdsFromTickets(){
     return array_unique( array_map( function( $ticket ){ return $ticket->getSpaceId(); }, $this->getCollection() ) );
   }
-  
+
 
   public function asArraySortedBySpaceId(){
     $space_tickets = array();
     $class_name = get_class($this);
 
-    // chunk out the tickets for each space                                                                     
+    // chunk out the tickets for each space
     foreach( $this->getCollection() AS $ticket ){
       if(! array_key_exists( $ticket->getSpaceId(), $space_tickets ) ){
         $space_tickets[$ticket->getSpaceId()] = new $class_name;
@@ -38,9 +38,9 @@ class Assembla_Collection_Ticket extends Assembla_Collection_Abstract {
 
 
   protected function _appendPagedReport( $id, $use_cache ){
-    $i = 0; 
-    do{ 
-      $i++; 
+    $i = 0;
+    do{
+      $i++;
       $length = count($this);
 
       $request = $this->_getAPI($use_cache)->generateRequest("paged_portfolio_ticket_report", array( $id, $i ) );
@@ -48,23 +48,23 @@ class Assembla_Collection_Ticket extends Assembla_Collection_Abstract {
 
       $element = new Assembla_API_XML_Element( $http_response );
       if( ! count($element->xpath('error')) ) {
-	$element = new Assembla_API_XML_Element( $http_response );
-	$this->load($element);
+        $element = new Assembla_API_XML_Element( $http_response );
+        $this->load($element);
       } else {
-	$errors = $element->asCanonicalArray();
-	$err = "Errors were encountered:";
-	foreach( $errors as $e ){
-	  if( $e == "HTTP Basic: Access denied." ){
-	    throw new Core_Exception_Auth( "Authentical credentials failed!" );
-	  } else {
-	    $err .= " " . $e . " ";
-	  }
-	}
-	throw new Exception( $err );
-	
-      }     
-      // asCanonicalArray will return "" if empty - this means 
-      // we're out of tickets so break the loop.  
+        $errors = $element->asCanonicalArray();
+        $err = "Errors were encountered:";
+        foreach( $errors as $e ){
+          if( $e == "HTTP Basic: Access denied." ){
+            throw new Core_Exception_Auth( "Authentical credentials failed!" );
+          } else {
+            $err .= " " . $e . " ";
+          }
+        }
+        throw new Exception( $err );
+
+      }
+      // asCanonicalArray will return "" if empty - this means
+      // we're out of tickets so break the loop.
     } while( (bool) $element->asCanonicalArray() );
   }
 
@@ -80,7 +80,7 @@ class Assembla_Collection_Ticket extends Assembla_Collection_Abstract {
     $this->_filters["statusFilter"] = array( "status" => $status );
     return $this;
   }
-  
+
 
   private function _convertTicketStatus( $status_element ){
     if( (string) $status_element == "Array" ):
@@ -89,15 +89,15 @@ class Assembla_Collection_Ticket extends Assembla_Collection_Abstract {
       return (string) $status_element;
     endif;
   }
-  
+
   protected function statusFilter( $element, $status ){
-    if( !is_array($status) ){ 
-      $status = array($status); 
+    if( !is_array($status) ){
+      $status = array($status);
     }
-    
+
     foreach( $status AS $s ){
       if( $this->_convertTicketStatus( $element->status ) == $s ){
-	return true;
+        return true;
       }
     }
 
@@ -106,21 +106,21 @@ class Assembla_Collection_Ticket extends Assembla_Collection_Abstract {
 
   public function setComponentNameOnTickets(){
 
-    foreach($this->getCollection() AS $ticket ){      
+    foreach($this->getCollection() AS $ticket ){
       $space_id = $ticket->getSpaceId();
       if( !in_array($space_id, array_keys( $this->_component_filter_cache ) ) ){
-	$space = new Blast_Assembla_Model_Space;                                                                                              
-	$space->loadById($space_id, true)->loadComponents(true);    
-	$this->_component_filter_cache[$space_id] = $space;
+        $space = new Blast_Assembla_Model_Space;
+        $space->loadById($space_id, true)->loadComponents(true);
+        $this->_component_filter_cache[$space_id] = $space;
       } else {
-	$space = $this->_component_filter_cache[$space_id];
+        $space = $this->_component_filter_cache[$space_id];
       }
-      
+
       $component_map = $space->getComponents()->getComponentIDNameMap();
       if($ticket->getComponentId() != '' && isset($component_map[$ticket->getComponentId()])){
-	$ticket->setComponent($component_map[$ticket->getComponentId()]);
+        $ticket->setComponent($component_map[$ticket->getComponentId()]);
       } else {
-	$ticket->setComponent('');
+        $ticket->setComponent('');
       }
     }
 
@@ -129,13 +129,13 @@ class Assembla_Collection_Ticket extends Assembla_Collection_Abstract {
   protected function excludeComponentFilter( $element, $components ){
     if( !is_array($components) ){
       $components = array($components);
-    }    
+    }
 
     $space_id = (string) $element->{'space-id'};
 
     if( !in_array($space_id, array_keys( $this->_component_filter_cache ) ) ){
-      $space = new Blast_Assembla_Model_Space;                                                                                              
-      $space->loadById($space_id, true)->loadComponents(true);    
+      $space = new Blast_Assembla_Model_Space;
+      $space->loadById($space_id, true)->loadComponents(true);
       $this->_component_filter_cache[$space_id] = $space;
     } else {
       $space = $this->_component_filter_cache[$space_id];
@@ -147,7 +147,7 @@ class Assembla_Collection_Ticket extends Assembla_Collection_Abstract {
     if( isset($component_map[$ticket_component]) ){
       return !in_array( $component_map[$ticket_component], $components);
     }
-    
+
     return true;
 
   }
@@ -159,32 +159,32 @@ class Assembla_Collection_Ticket extends Assembla_Collection_Abstract {
 
 
   protected function excludeStatusFilter( $element, $status ){
-    if( !is_array($status) ){ 
-      $status = array($status); 
+    if( !is_array($status) ){
+      $status = array($status);
     }
-    
+
     if( !in_array( $this->_convertTicketStatus( $element->status ), $status ) ){
       return true;
     }
 
     return false;
   }
-  
+
 
   public function addExcludeStatusFilter( $status ){
     $this->_filters["excludeStatusFilter"] = array( "status" => $status );
     return $this;
   }
-  
 
-  
-  // Due Date filter                                                                                                                                                                     
+
+
+  // Due Date filter
   public function addHasDueDateFilter(){
     $this->_filters['hasDueDateFilter'] = array();
     return $this;
   }
-  
-  protected function hasDueDateFilter( $element ){    
+
+  protected function hasDueDateFilter( $element ){
     if( $element->xpath(".//custom-field[@name='Due Date']") ){
       return true;
     } else {
@@ -192,23 +192,23 @@ class Assembla_Collection_Ticket extends Assembla_Collection_Abstract {
     }
   }
 
-  // Date Filter functions                                                                                                                                                               
+  // Date Filter functions
   public function addDateAfterFilter( $date, $date_string ){
     $this->_filters["dateAfter"] = array( "date" => $date, "date_string" => $date_string );
     return $this;
   }
-  
-  
+
+
   protected function dateAfter( $element, $date, $date_string ){
     $element_date = new DateTime( (string) $element->$date_string );
     return (bool)( $element_date->getTimeStamp() > $date->getTimeStamp() );
   }
-  
+
   public function addDateBeforeFilter( $date, $date_string ){
     $this->_filters["dateBefore"] = array( "date" => $date, "date_string" => $date_string );
     return $this;
   }
-  
+
   protected function dateBefore( $element, $date, $date_string ){
     $element_date = new DateTime( (string) $element->$date_string );
     return (bool)( $element_date->getTimeStamp() < $date->getTimeStamp() );
@@ -218,18 +218,18 @@ class Assembla_Collection_Ticket extends Assembla_Collection_Abstract {
     $this->_filters["lastNumDaysFilter"] = array( "days" => $days, "date_string" => $date_string );
     return $this;
   }
-  
+
   protected function lastNumDaysFilter( $element, $days, $date_string ){
     $element_date = new DateTime( (string) $element->$date_string );
     $interval = $element_date->diff( new DateTime('') );
-    
+
     return (bool)( $interval->days < $days );
-    
+
   }
-  
+
   public function addNotTodayFilter( $date_string ){
     $this->_filters["notToday"] = array( 'date_string' => $date_string,
-					 'today' => date('Ymd', time()) );
+                                         'today' => date('Ymd', time()) );
     return $this;
   }
 
@@ -240,7 +240,7 @@ class Assembla_Collection_Ticket extends Assembla_Collection_Abstract {
     return $d->format('Ymd') != $today;
   }
 
-  }
+}
 
 
 ?>
